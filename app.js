2cde3c929aae23eaa3d3ef570f84512f942b3830
jumael99 <jumael99@gmail.com>
2024-03-04 12:35:03 +0600

    added ejs-mate for easy layouts

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 const CampGround = require('./model/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
@@ -17,6 +18,7 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
